fa6c4531addd27d7e9488e3400bfd1815d38ff8d
Meower125er <sahil.sinhakol1@gmail.com>
2024-03-30 16:06:46 +0530

    break and continue

--- for loops/main.cpp
+++ for loops/main.cpp
@@ -6,5 +6,27 @@ int main() {
     for(int i=1; i<=10; i++) {
         cout<<i<<endl;
     }
-    cout<<"meow";
+    cout<<"meow"<<endl;
+
+    for (int i = 1; i <= 20; i++)
+    {
+        if (i==13)
+        {
+            break;
+        }
+        cout<<i<<endl;
+    }
+
+    for (int i = 1; i <= 20; i++)
+    {
+        if (i==13)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+        
+    }
+    
+    
+
 }
